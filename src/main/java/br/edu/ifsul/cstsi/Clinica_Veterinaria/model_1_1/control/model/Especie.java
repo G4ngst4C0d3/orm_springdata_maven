99c8cb645349684a0dbcdb4e4d57e47bb8eaaced
Anderson Leite <andersonleite.pl013@academico.ifsul.edu.br>
2023-11-23 16:55:56 -0300

    Mapeamento da tabela especie

--- src/main/java/br/edu/ifsul/cstsi/Clinica_Veterinaria/model_1_1/control/model/Especie.java
+++ src/main/java/br/edu/ifsul/cstsi/Clinica_Veterinaria/model_1_1/control/model/Especie.java
@@ -1,5 +1,21 @@
 package br.edu.ifsul.cstsi.Clinica_Veterinaria.model_1_1.control.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "Especie")
+@Table(name = "especies")
 public class Especie {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
     private String nome_especie;
+
+    @OneToMany
+    private List <Animal> animais;
 }
